61947526cea87b14d052548d1ca37dfe7d97cad6
maelog <mael.dabard@gmail.com>
2023-10-05 12:32:07 +0200

    Generate Coord of all mine with the value

--- Game.py
+++ Game.py
@@ -1,7 +1,8 @@
 import AskInputs
+import random
 
-#Function to Init Game
-#ask all information for the game, size / number of mines
+# Function to Init Game
+# ask all information for the game, size / number of mines
 def InitGame():
     print(""" __        __   _                            _          __  __ _            ____                                   
  \ \      / /__| | ___ ___  _ __ ___   ___  | |_ ___   |  \/  (_)_ __   ___/ ___|_      _____  ___ _ __   ___ _ __ 
@@ -9,15 +10,35 @@ def InitGame():
    \ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) | | |  | | | | | |  __/___) \ V  V /  __/  __/ |_) |  __/ |   
     \_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/  |_|  |_|_|_| |_|\___|____/ \_/\_/ \___|\___| .__/ \___|_|   
                                                                                                   |_|              """)
-    print(120*"*")
+    print(120 * "*")
 
     width = AskInputs.AskInputInt("Enter field width : ")
     height = AskInputs.AskInputInt("Enter field height : ")
     number_mines = AskInputs.AskInputInt("How many mines ? ")
     return {"width": width, "height": height, "number_mines": number_mines}
 
+
+def GenerateGame(value):
+    coord_mine = GenerateCoordMine(value)
+    map = GenerateMap(value, coord_mine)
+    print(map)
+
+
+def GenerateCoordMine(value):
+    coordinates = set()
+    x,y = random.randint(value["width"], value["height"]), random.randint(value["width"], value["height"])
+    for nbr in range(value["number_mines"]):
+        coordinates.add((x,y))
+        x,y = random.randint(value["width"], value["height"]), random.randint(value["width"], value["height"])
+    return coordinates
+
+def GenerateMap(value, coord_mine):
+
+
+
 def Game():
     value_game = InitGame()
-    print(value_game)
+    GenerateGame(value_game)
+
 
 Game()
